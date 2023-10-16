a010e5b4eca6211a9613193fc6c44d8478a37390
Ajeet Gill <ajeetshergill@gmail.com>
2023-10-16 04:59:45 -0300

    update db url

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
 }
 const animeAPIUrl = `${
     isLocalhost
-    ? "http://localhost:4000"
-    : "https://anime-facts-rest-api.herokuapp.com"
+        ? "http://localhost:4000"
+        : "https://anime-facts-rest-api-alpha.vercel.app"
 }/api/v1`;
 
-
+console.log(`isLocalhost: ${isLocalhost}`);
 let animeList;
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -26,12 +26,10 @@ function onLoad() {
     renderAnimeCards();
     document.body.addEventListener("keyup", fadeOtherElementsExceptMeKeyboard);
     // document.body.addEventListener("keyup", removeFadeOtherElementsExceptMeKeyboard);
-    document.querySelectorAll(`.card`).forEach(el => {
+    document.querySelectorAll(`.card`).forEach((el) => {
         el.addEventListener("mouseenter", fadeOtherElementsExceptMe);
         el.addEventListener("mouseleave", removeFadeOtherElementsExceptMe);
     });
-
-
 }
 
 async function getRandomFact(animeID) {
@@ -50,29 +48,31 @@ async function getRandomFact(animeID) {
     return randomFacts[randomNumber].fact;
 }
 
-
 function showRandomFact(e) {
     let animeID = e.id;
-    const animeName = document.querySelector(`#${animeID} .card__title`).innerText;
+    const animeName = document.querySelector(
+        `#${animeID} .card__title`
+    ).innerText;
     let htmlRandomFact = document.querySelector(`.random__fact`);
     let htmlRandomName = document.querySelector(`.random__name`);
     // at this point I'm sure AnimeList is in local storage for sure
     // get the ID from local storage then get random fact from API
+
     // getFact(animeID) returns the fact to be rendered
 
-    getRandomFact(animeID).then(fact => {
+    getRandomFact(animeID).then((fact) => {
         htmlRandomName.innerHTML = animeName;
-        htmlRandomFact.innerHTML = fact
-    })
+        htmlRandomFact.innerHTML = fact;
+    });
 }
 async function fetchAndStoreRandomFacts(animeID) {
     let f = await fetch(`${animeAPIUrl}/${animeID}`).catch((e) => {
         console.error(e);
         console.error(`caught error-2`);
     });
-    await f.json().then(facts => {
-        localStorage.setItem(animeID, JSON.stringify(facts.data))
-    })
+    await f.json().then((facts) => {
+        localStorage.setItem(animeID, JSON.stringify(facts.data));
+    });
 }
 
 async function fetchAnimeList() {
@@ -80,25 +80,26 @@ async function fetchAnimeList() {
         console.error(e);
         console.error(`caught error-3`);
     });
-    let result = await r.json().then((e) => { return e.data });
+    let result = await r.json().then((e) => {
+        return e.data;
+    });
     return result;
 }
 
 async function renderAnimeCards() {
-    let animeList = '';
+    let animeList = "";
     // localStorage.removeItem('animeList');
-    if (localStorage.getItem('animeList')) {
+    if (localStorage.getItem("animeList")) {
         // console.log('fetching list from localStorage');
-        animeList = JSON.parse(localStorage.getItem('animeList'));
-    }
-    else {
+        animeList = JSON.parse(localStorage.getItem("animeList"));
+    } else {
         // console.log('fetching list from API');
         animeList = await fetchAnimeList();
-        localStorage.setItem('animeList', JSON.stringify(animeList));
+        localStorage.setItem("animeList", JSON.stringify(animeList));
     }
-    const htmlResults = document.getElementById('allAnime');
-    animeList.forEach(anime => {
-        let name = anime.anime_name.toString().replaceAll('_', ' ');
+    const htmlResults = document.getElementById("allAnime");
+    animeList.forEach((anime) => {
+        let name = anime.anime_name.toString().replaceAll("_", " ");
         let imgUrl = anime.anime_img;
         name = capitalizeFirstLetter(name);
         const cardTemplate = `<button id=${anime.anime_name} onclick=showRandomFact(this) class="card">
@@ -107,17 +108,16 @@ async function renderAnimeCards() {
                                 </button>`;
         htmlResults.innerHTML += cardTemplate;
     });
-
 }
 
 function clearLocalStorage() {
-    console.log('cleared local storage :)');
+    console.log("cleared local storage :)");
     document.querySelector(`.random__name`).innerHTML = `One Piece`;
-    document.querySelector(`.random__fact`).innerHTML = `It's best anime ever🧡`;
+    document.querySelector(
+        `.random__fact`
+    ).innerHTML = `It's best anime ever🧡`;
     localStorage.clear();
 }
-
-
 
 // Start : Fade all cards except the one that's hovering over
 // applies an effect to all elements with the same class within the parent
@@ -128,21 +128,20 @@ function fadeOtherElementsExceptMe(e) {
     const activelyHoveredElementId = e.target.id;
 
     // // apply effect on all elements except present element
-    document.querySelectorAll(commonSiblingSelector).forEach(el => {
+    document.querySelectorAll(commonSiblingSelector).forEach((el) => {
         // console.log(el.id);
         if (activelyHoveredElementId !== el.id)
-            el.style.filter = 'blur(2px) grayscale(.5)';
-    })
-
+            el.style.filter = "blur(2px) grayscale(.5)";
+    });
 }
 function removeFadeOtherElementsExceptMe(e) {
     const parentId = e.srcElement.parentNode.id;
     const commonSiblingClass = e.target.className;
     const commonSiblingSelector = `#${parentId} .${commonSiblingClass}`;
 
-    document.querySelectorAll(commonSiblingSelector).forEach(el => {
-        el.style.filter = 'blur(0px) grayscale(0)'
-    })
+    document.querySelectorAll(commonSiblingSelector).forEach((el) => {
+        el.style.filter = "blur(0px) grayscale(0)";
+    });
 }
 
 function fadeOtherElementsExceptMeKeyboard(e) {
@@ -151,22 +150,20 @@ function fadeOtherElementsExceptMeKeyboard(e) {
     const commonSiblingSelector = `#${parentId} .${commonSiblingClass}`;
     const activelyFocusedElementId = e.target.id;
 
-    if (document.activeElement.className === 'card') {
+    if (document.activeElement.className === "card") {
         // Blur other elements except me
-        document.querySelectorAll(commonSiblingSelector).forEach(el => {
-            el.style.filter = 'blur(0) grayscale(0)';
-        })
+        document.querySelectorAll(commonSiblingSelector).forEach((el) => {
+            el.style.filter = "blur(0) grayscale(0)";
+        });
 
-        document.querySelectorAll(commonSiblingSelector).forEach(el => {
+        document.querySelectorAll(commonSiblingSelector).forEach((el) => {
             if (activelyFocusedElementId !== el.id)
-                el.style.filter = 'blur(2px) grayscale(.5)';
-        })
+                el.style.filter = "blur(2px) grayscale(.5)";
+        });
+    } else {
+        document.querySelectorAll(".card").forEach((el) => {
+            el.style.filter = "blur(0) grayscale(0)";
+        });
     }
-    else {
-        document.querySelectorAll('.card').forEach(el => {
-            el.style.filter = 'blur(0) grayscale(0)';
-        })
-    }
-
 }
 // End : Fade all cards except the one that's hovering over
